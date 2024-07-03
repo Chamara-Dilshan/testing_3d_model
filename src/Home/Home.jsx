c224f093a600a02e10f69d5754804c6179ffcc46
Chamara Dilshan <chamaradilshan0011@gmail.com>
2024-07-03 19:17:54 +0530

    1st

---
+++ src/Home/Home.jsx
@@ -0,0 +1,18 @@
+import './Home.css';
+import { Link } from 'react-router-dom';
+
+function Home() {
+  return (
+    <div className="App">
+      <div className="centered-container">
+        <ul className="centered-list">
+          <li><Link to="/test1">Test 1</Link></li>
+          <li>Test 2</li>
+          <li>Test 3</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
