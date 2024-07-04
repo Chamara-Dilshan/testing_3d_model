072624485149be2bbecf4b2288f5a7ff4506a5ab
Chamara Dilshan <chamaradilshan0011@gmail.com>
2024-07-04 18:50:21 +0530

    updated

--- src/Test_1/Test_1.jsx
+++ src/Test_1/Test_1.jsx
@@ -1,13 +1,43 @@
 import { Canvas } from '@react-three/fiber'
 import React from 'react'
+import {useState} from 'react'
 import Test1_Scene from '../Components/Test1_Scene'
 import './Test_1.css';
 
 const Test_1 = () => {
+  const [mainColor, setMainColor] = useState('#e66465');
+
+    const handleMainColorChange = (event) => {
+        setMainColor(event.target.value);
+    };
+
   return (
-    <Canvas>
-        <Test1_Scene/>
-   </Canvas>
+    <div>
+        <div className="wrapper">
+            <div className="card">
+                <div className="product-canvas">
+                    <Canvas>
+                    <Test1_Scene color={mainColor}/>
+                    </Canvas>
+                </div>
+                <h2>Color chooser</h2>
+                <div className='colors'>
+                    <div>
+                        <input 
+                            type="color" 
+                            id="head" 
+                            name="head"
+                            value={mainColor}
+                            onChange={handleMainColorChange}
+                        />
+                        <label for="head">Main Color</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+     
+
+    </div>
   )
 }
 
--- src/Test_2/Test_2.jsx
+++ src/Test_2/Test_2.jsx
@@ -3,15 +3,8 @@ import './Test_2.css';
 import {Suspense, useRef,useState} from 'react'
 import { Canvas} from '@react-three/fiber'
 import {OrbitControls, useGLTF} from '@react-three/drei'
-import Test1_Scene from '../Components/Test1_Scene'
 
 function Test_2() {
-
-    const [mainColor, setMainColor] = useState('#e66465');
-
-    const handleMainColorChange = (event) => {
-        setMainColor(event.target.value);
-    };
 
   return (
     <div>
@@ -19,7 +12,6 @@ function Test_2() {
             <div className="card">
                 <div className="product-canvas">
                     <Canvas>
-                    <Test1_Scene color={mainColor}/>
                     </Canvas>
                 </div>
                 <h2>Color chooser</h2>
@@ -29,23 +21,29 @@ function Test_2() {
                             type="color" 
                             id="head" 
                             name="head"
-                            // value="#e66465" 
-                            value={mainColor}
-                            onChange={handleMainColorChange}
+                            value="#e66465" 
                         />
                         <label for="head">Main Color</label>
                     </div>
 
-                    {/* <div>
-                        <input type="color" id="body" name="body"
-                                value="#f6b73c" />
+                    <div>
+                        <input 
+                            type="color" 
+                            id="body" 
+                            name="body"
+                            value="#f6b73c" 
+                        />
                         <label for="body">Stripes</label>
                     </div>
                     <div>
-                        <input type="color" id="body" name="body"
-                                value="#f6b73c" />
+                        <input 
+                            type="color" 
+                            id="body" 
+                            name="body" 
+                            value="#f6b73c" 
+                        />
                         <label for="body">Soul</label>
-                    </div> */}
+                    </div>
                 </div>
             </div>
         </div>
